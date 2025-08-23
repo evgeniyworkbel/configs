66d22b7e0a2a1d1a0ab49be42feca103e2fbff3f
Evgeniy Samuta <evgeniyworkbel@gmail.com>
2025-08-24 00:45:03 +0300

    feat: extend tailwindAttributes, add comments

--- packages/prettier-config/index.js
+++ packages/prettier-config/index.js
@@ -1,9 +1,10 @@
 export default {
   plugins: ["prettier-plugin-tailwindcss"],
 
-  // Settings provided for prettier-plugin-tailwindcss (https://github.com/tailwindlabs/prettier-plugin-tailwindcss)
-  // For Tailwind v4, specify path to import tailwind using tailwindStylesheet
+  /* Settings provided for prettier-plugin-tailwindcss (https://github.com/tailwindlabs/prettier-plugin-tailwindcss) */
+  // For Tailwind v4, do not forget to specify path to importing tailwind using 'tailwindStylesheet' property
   // tailwindStylesheet: "./src/app/global.css",
-  tailwindAttributes: ["className"],
+  // Track the feature-request https://github.com/tailwindlabs/prettier-plugin-tailwindcss/issues/373. Replace array of many attributes names with simple regex
+  tailwindAttributes: ["className", "wrapperClassName", "titleClassName"],
   tailwindFunctions: ["cn", "clsx", "cva"],
 };
