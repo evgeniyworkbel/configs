3936add45da8a687d43e2d35df59d6506858ed77
Evgeniy Samuta <evgeniyworkbel@gmail.com>
2025-08-24 01:24:56 +0300

    build: extend rules of stylelint config

--- packages/stylelint-config/index.js
+++ packages/stylelint-config/index.js
@@ -4,5 +4,8 @@ export default {
     "function-disallowed-list": ["rgba", "hsla", "rgb", "hsl"],
     "color-function-notation": "modern",
     "color-no-hex": true,
+    "import-notation": null,
+    "at-rule-no-unknown": [true, { ignoreAtRules: ["theme"] }],
+    "lightness-notation": "number",
   },
 };
